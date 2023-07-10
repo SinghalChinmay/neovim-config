113bb21362e147ad325a474b78aa9344295db034
Chinmay Singhal <chinmay.singhal08@gmail.com>
2023-07-10 18:59:32 +0530

    remove treesitter for help pages

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require ('nvim-treesitter.configs').setup {
 	-- A list of parser names, or "all"
-	ensure_installed = {"help", "javascript", "typescript", "python", "c", "lua", "rust" },
+	ensure_installed = {"javascript", "typescript", "python", "c", "lua", "rust" },
 
 	sync_install = false,
 
